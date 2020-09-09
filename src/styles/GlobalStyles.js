695ccfab98a62bb851512fb27ef61bd725b29add
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-08 23:15:27 -0300

    remove colors and fix theme name

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -6,7 +6,7 @@ export default createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
 
-    color: ${props => props.theme.colors.text};
+    color: ${props => props.theme.colors.color};
   }
 
   html, body, #root {
@@ -24,20 +24,6 @@ export default createGlobalStyle`
   }
 
   html {
-    background: ${props => props.theme.colors.background};
-  }
-
-  :root {
-    --primary: #000;
-    --secondary: #15181C;
-    --search: #202327;
-    --white: #D9D9D9;
-    --gray: #7A7A7A;
-    --outline: #2F3336;
-    --retweet: #00C06B;
-    --like: #E8265E;
-    --twitter: #33A1F2;
-    --twitter-dark-hover: #011017;
-    --twitter-light-hover: #2C8ED6;
+    background: ${props => props.theme.colors.primary};
   }
 `

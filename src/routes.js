3e3e03b00fcae74ba5993f89c110907b39c4cdd8
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-09 20:09:54 -0300

    add signin route

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/signup" component={SignUp} />
+        <Route path="/signin" component={SignIn} />
       </Switch>
     </BrowserRouter>
   )

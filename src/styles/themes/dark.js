20a905c55b3c945065a943bc4293c06a99fa4bae
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-08 23:18:06 -0300

    fix dark colors

--- src/styles/themes/dark.js
+++ src/styles/themes/dark.js
@@ -2,7 +2,17 @@ export default {
   title: 'dark',
 
   colors: {
-    background: '#000',
-    text: '#fff'
+    primary: '#000',
+    secondary: '#15181C',
+    search: '#202327',
+    white: '#D9D9D9',
+    color: '#FFF',
+    gray: '#7A7A7A',
+    outline: '#2F3336',
+    retweet: '#00C06B',
+    like: '#E8265E',
+    twitter: '#33A1F2',
+    twitterDarkHover: '#011017',
+    twitterLightHover: '#2C8ED6'
   }
 }

bdae7afd2dae42f8a19d6ea10f7f3e0ff1191982
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-08 23:18:29 -0300

    add temporary light colors

--- src/styles/themes/light.js
+++ src/styles/themes/light.js
@@ -2,7 +2,17 @@ export default {
   title: 'light',
 
   colors: {
-    background: '#fff',
-    text: '#000'
+    primary: '#FFF',
+    secondary: '#15181C',
+    search: '#202327',
+    white: '#D9D9D9',
+    color: '#000',
+    gray: '#7A7A7A',
+    outline: '#2F3336',
+    retweet: '#00C06B',
+    like: '#E8265E',
+    twitter: '#33A1F2',
+    twitterDarkHover: '#011017',
+    twitterLightHover: '#2C8ED6'
   }
 }

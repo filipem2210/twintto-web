cea2501a141a247580510cb2c744466ead40b14d
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-10 17:59:37 -0300

    fix text alignment

--- src/components/Tabs/styles.js
+++ src/components/Tabs/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   justify-content: space-between;
+  text-align: center;
   border-bottom: 1px solid ${props => props.theme.colors.outline};
   margin-top: 10px;
 `

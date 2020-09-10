e7168f1db15673987ddcf5ca8eacaaff284a40ae
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-10 13:28:41 -0300

    fix color

--- src/components/SideBarList/styles.js
+++ src/components/SideBarList/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  background: var(--secondary);
+  background: ${props => props.theme.colors.primary};
   border-radius: 14px;
 `
 

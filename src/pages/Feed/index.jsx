9243a0a618fbb56b23edb88493a0c38abff87044
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-10 00:37:00 -0300

    add components and styles to feed page

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 
+import MenuBar from '../../components/MenuBar'
+
+import { Container, Wrapper } from './styles'
+
 export default function Feed() {
-  return <h1>Feed</h1>
+  return (
+    <Container>
+      <Wrapper>
+        <MenuBar />
+      </Wrapper>
+    </Container>
+  )
 }

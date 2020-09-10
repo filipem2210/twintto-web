4cd9ff2f7f643025ebce3f42f144ba2ced88e466
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-10 14:52:17 -0300

    fix colors

--- src/styles/themes/dark.js
+++ src/styles/themes/dark.js
@@ -4,7 +4,9 @@ export default {
   colors: {
     primary: '#000',
     secondary: '#15181C',
-    search: '#202327',
+    sidebarBg: '#202327',
+    sidebarHover: 'rgba(255, 255, 255, 0.03)',
+    sidebarSearch: '#202327',
     white: '#D9D9D9',
     trueWhite: '#FFF',
     text: '#FFF',
@@ -15,6 +17,7 @@ export default {
     twitter: '#33A1F2',
     hover: '#011017',
     hoverButton: '#1a91da',
+    hoverOutlineButton: 'rgba(29, 161, 242, 0.1)',
     boxShadow: 'rgba(255, 255, 255, 0.2)'
   }
 }
--- src/styles/themes/light.js
+++ src/styles/themes/light.js
@@ -4,7 +4,9 @@ export default {
   colors: {
     primary: '#FFF',
     secondary: '#15181C',
-    search: '#202327',
+    sidebarBg: 'rgb(245, 248, 250)',
+    sidebarHover: 'rgba(0, 0, 0, 0.03)',
+    sidebarSearch: 'rgb(230, 236, 240)',
     white: '#D9D9D9',
     trueWhite: '#FFF',
     text: '#000',
@@ -15,6 +17,7 @@ export default {
     twitter: '#33A1F2',
     hover: 'rgba(29, 161, 242, 0.1)',
     hoverButton: '#1a91da',
+    hoverOutlineButton: 'rgba(29, 161, 242, 0.1)',
     boxShadow: 'rgba(0, 0, 0, 0.2)'
   }
 }

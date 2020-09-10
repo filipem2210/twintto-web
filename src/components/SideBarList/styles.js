fd7e54884f7f73bef1a137e8feb0559e8cfdd6af
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-10 14:52:53 -0300

    add styles

--- src/components/SideBarList/styles.js
+++ src/components/SideBarList/styles.js
@@ -3,12 +3,25 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  background: ${props => props.theme.colors.primary};
+  background-color: ${props => props.theme.colors.sidebarBg};
   border-radius: 14px;
 `
 
 export const Item = styled.div`
   padding: 10px 16px;
+  transition-property: background-color;
+  transition-duration: 0.2s;
+
+  div + &:hover {
+    cursor: pointer;
+    background-color: ${props => props.theme.colors.sidebarHover};
+  }
+
+  strong {
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 
   & + div {
     border-top: 1px solid ${props => props.theme.colors.outline};

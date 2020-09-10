50c458c0bd7f6da607d7b12901c75d216cac6ab2
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-10 17:24:10 -0300

    add feed component to feed page

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
 import MenuBar from '../../components/MenuBar'
+import Feed from '../../components/Feed'
 import SideBar from '../../components/SideBar'
 
 import { Container, Wrapper } from './styles'
 
-export default function Feed() {
+export default function FeedPage() {
   return (
     <Container>
       <Wrapper>
         <MenuBar />
+        <Feed />
         <SideBar />
       </Wrapper>
     </Container>

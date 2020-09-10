8210b45cc0da9ecd020bb23e8c287fe815565313
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-10 19:50:36 -0300

    add avatar and fix styles

--- src/components/FeedMain/index.jsx
+++ src/components/FeedMain/index.jsx
@@ -4,12 +4,13 @@ import Post from '../Post'
 import Feed from '../Feed'
 import BottomMenu from '../BottomMenu'
 
-import { Container, Header, Title } from './styles'
+import { Container, Header, Avatar, Title } from './styles'
 
 export default function FeedMain() {
   return (
     <Container>
       <Header>
+        <Avatar src="https://pbs.twimg.com/profile_images/1260607790323830791/NGrNpCkO_bigger.jpg" />
         <Title>Página Inicial</Title>
       </Header>
       <Post />
--- src/components/FeedMain/styles.js
+++ src/components/FeedMain/styles.js
@@ -5,8 +5,11 @@ export const Container = styled.div`
   flex-direction: column;
   width: 100%;
   max-width: 600px;
-  border-right: solid 1px ${props => props.theme.colors.outline};
-  border-left: solid 1px ${props => props.theme.colors.outline};
+
+  @media (min-width: 500px) {
+    border-right: solid 1px ${props => props.theme.colors.outline};
+    border-left: solid 1px ${props => props.theme.colors.outline};
+  }
 `
 
 export const Header = styled.div`
@@ -18,6 +21,19 @@ export const Header = styled.div`
 
   &:hover {
     cursor: pointer;
+  }
+`
+export const Avatar = styled.img`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 30px;
+  width: 30px;
+  margin-right: 10px;
+  border-radius: 50%;
+
+  @media (min-width: 400px) {
+    display: none;
   }
 `
 

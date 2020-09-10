94e2159cd00528b40e6e79cead1a816e51c5a2cc
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-09-10 19:40:42 -0300

    add feedmain component

--- src/components/FeedMain/index.jsx
+++ src/components/FeedMain/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
+import Post from '../Post'
 import Feed from '../Feed'
 import BottomMenu from '../BottomMenu'
 
-import { Container } from './styles'
+import { Container, Header, Title } from './styles'
 
 export default function FeedMain() {
   return (
     <Container>
+      <Header>
+        <Title>Página Inicial</Title>
+      </Header>
+      <Post />
       <Feed />
       <BottomMenu />
     </Container>
--- src/components/FeedMain/styles.js
+++ src/components/FeedMain/styles.js
@@ -6,4 +6,22 @@ export const Container = styled.div`
   width: 100%;
   max-width: 600px;
   border-right: solid 1px ${props => props.theme.colors.outline};
+  border-left: solid 1px ${props => props.theme.colors.outline};
+`
+
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  border-bottom: solid 1px ${props => props.theme.colors.outline};
+  height: 53px;
+  padding-left: 15px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+export const Title = styled.h2`
+  font-size: 20px;
+  font-weight: bold;
 `
